ca451f66e722570d8593adbe2699d6ca93796bc1
Gousiya <gousiya611shaik@gmail.com>
2023-04-24 11:43:20 +0530

    lcm.py

---
+++ lcm.py
@@ -0,0 +1,13 @@
+def lcm(a,b):
+    a=int(input())
+    b=int(input())
+    if a>b:
+        min=a
+    else:
+        min=b
+    while(1):
+        if min%a==0 and min%b==0:
+            lcm=min
+        min+=1
+    return lcm
+lcm(13,15)
